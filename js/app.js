4a841079a1a7a3628c83fcd031cd51bf509a778b
Enas Hamdy <36416682+EnasHamdy@users.noreply.github.com>
2021-10-21 04:54:13 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -23,10 +23,13 @@
  * 
 */
 // Create a variable to hold all sections in the page.
-const allSections = document.querySelectorAll('section');
+const allSections = document.querySelectorAll("section");
 
-// Get Navbar menu list
-let navbarMenuList = document.getElementById("navbar__list");
+// Get Navbar menu list.
+let navbarMenuList = document.querySelector("#navbar__list");
+
+// Set the delay time before hiding the navigation bar.
+let timer = null;
 
 /**
  * End Global Variables
@@ -37,12 +40,12 @@ let navbarMenuList = document.getElementById("navbar__list");
  * 
  * This function checks if an element is in the viewport or not
  * @param {*} secElement is the element to check its visibility
- * @returns the result of check (boolean)
+ * @returns the result of check : boolean
  */
 function isInView(secElement){  
-    // Get the bounds of the element
+    // 1 - Get the bounds of the element
     var bounding = secElement.getBoundingClientRect();
-    // Check if the element is in the viewport or not
+    // 2 - Check if the element is in the viewport or not
     if (
         (bounding.top >= 0) &&
         bounding.left >= 0 &&
@@ -55,7 +58,6 @@ function isInView(secElement){
     }
 }
 
-
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -63,12 +65,15 @@ function isInView(secElement){
 */
 
 // build the nav
+/**
+ * This function builds the navigation bar dynamically according to the number of sections on the page.
+ */
 function buildNavBar(){
-    for (let i=0; i<allSections.length; i++){
+    for (let i = 0; i < allSections.length; i++){
         // 1 - Create li: Navbar list item
         const newListItem = document.createElement("li");
         
-        // 2 - Create a: list item link and set its attributes
+        // 2 - Create a: list item anchor link and set its attributes
         const newLink = document.createElement("a");    
         newLink.innerText = allSections[i].getAttribute("data-nav");
         newLink.className = "menu__link";
@@ -82,11 +87,105 @@ function buildNavBar(){
 }
 
 // Add class 'active' to section when near top of viewport
-
-
+/**
+ * This function adds active class to the section if it is in the viewport .
+ */
+function checkActiveSection(){
+    for (let oneSection of allSections){
+        // Sets a variable and assigns the result of isInView() method to it
+        let viewStatus = isInView(oneSection);
+        // If the result is true, set a section as active and activate the corresponding link
+        if(viewStatus === true){
+            // 1 - Set section as active            
+            oneSection.classList.add("your-active-class");
+            // 2 - Make navbar link active
+            setNavLinkActive(oneSection);            
+        } else {
+            // Remove active class from a section
+            oneSection.classList.remove("your-active-class");
+        }
+    }
+}
 
 // Scroll to anchor ID using scrollTO event
+/**
+ * This function gets the corresponding section of the clicked link
+ * and scrolls to it in a smooth behavior.
+ * @param {*} navLink the navbar clicked link
+ */
+ function scrollToSection(navLink){           
+    // 1 - Get the corresponding section of the clicked link
+    for (let oneSection of allSections){
+        if (navLink.innerText === oneSection.getAttribute("data-nav")){  
+            // 2 - Scroll to the section with smooth behavior
+            oneSection.scrollIntoView({behavior: "smooth"});
+        }
+    }
+}
 
+/**
+ * This function activates the navbar link if the corresponding section is in the viewport.
+ * @param {*} secElement is the section which in the viewport
+ */
+ function setNavLinkActive(secElement){
+    for (let navLink of navbarAnchorLinks){
+        // 1 - Get the corresponding navigation bar link
+        if(secElement.getAttribute("data-nav") === navLink.innerText){
+            // 2 - Set the link as active
+            navLink.setAttribute('style', 'background: #333; color: #fff; transition: ease 0.3s all');
+        }
+        else {
+            // Remove active styles if it isn't the desired link
+            navLink.removeAttribute('style', 'background: #333; color: #fff; transition: ease 0.3s all');
+        }
+    }
+}
+
+/**
+ * This function creates a scroll to top button on the page, 
+ * and set its id, text and styles.
+ */
+function createToTopButton(){
+    // 1 - Create a new button
+    const toTopBtn = document.createElement("button");
+    // 2 - Set button id
+    toTopBtn.setAttribute('id', "toTopId"); 
+    // 3 - Set button text
+    toTopBtn.innerText = "Top"; 
+    // 4 - Set styles of the button
+    toTopBtn.setAttribute("style", "display: none; position: fixed; bottom: 20px; right: 30px; z-index: 99; border: none; outline: none; background-color: red; color: white; cursor: pointer; padding: 15px; border-radius: 10px; font-size: 18px;");
+    // 5 - Append the button to the page
+    document.body.appendChild(toTopBtn);
+}
+
+/**
+ * This function defines whether or not to display the scroll to top button
+ */
+function scrollToTopButton() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      toTopBtn.style.display = "block";
+    } else {
+        toTopBtn.style.display = "none";
+    }
+  }
+
+  // Hide fixed navigation bar
+  /**
+   * This function hides fixed navigation bar while not scrolling.
+   * If a new scroll event occurred in the meantime, the timer is aborted, a new one is created 
+   * and the navigation bar will be displayed. If not, the navigation bar will hide.
+   */
+function hideNavBar(){    
+    if(timer !== null) {
+        // Display navigation bar
+        document.querySelector(".navbar__menu").style.display = "initial"; 
+        clearTimeout(timer);
+    }
+    // Hide navigation bar after 4seconds
+    timer = setTimeout(function() {       
+       document.querySelector(".navbar__menu").style.display = "none";
+    }, 4000);
+}
 
 /**
  * End Main Functions
@@ -94,24 +193,53 @@ function buildNavBar(){
  * 
 */
 
-// Build menu 
+// Build menu
 buildNavBar();
-
 
 // Scroll to section on link click
 
+// 1 - Get Navbar links
+let navbarAnchorLinks = document.querySelectorAll("a");
+// 2 - Add Click event listener to the link to scroll to the corressponding section
+for (let navLink of navbarAnchorLinks){
+    navLink.addEventListener('click', function(){
+        scrollToSection(navLink);
+    });
+}
 
-// Set sections as active        
-document.addEventListener("scroll", function(){
-    for (let oneSection of allSections){
-        // Sets a variable and assigns the result of isInView() method to it
-        let viewStatus = isInView(oneSection);
-        // If the result is true, set a section as active
-        if(viewStatus === true){
-            oneSection.className = "your-active-class";
-        } else {
-            oneSection.className = "";
-        }
+// Create scroll to top button on Page load
+/**
+ * Add event listener to the page to create 
+ * the scroll to top button on load.
+ */
+document.body.addEventListener("load", createToTopButton());
+
+/**
+ * // Add event listener to the scroll to top button
+ * When clicking on it, the page is scrolled to the top.
+ */
+let toTopBtn = document.querySelector("#toTopId");
+toTopBtn.addEventListener("click", function(){
+    // 1 - Scroll to the top of the page
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+
+    // 2 - Deactivate navbar links (as default)
+    for(let link of navbarAnchorLinks){
+        link.removeAttribute('style', 'background: #333; color: #fff; transition: ease 0.3s all');
     }
+    // 3 - Display navigation bar
+    document.querySelector(".navbar__menu").style.display = "initial"; 
 });
 
+// Add scroll event listener to the page
+document.addEventListener("scroll", function(){
+    // 1 - Display scroll up to top button
+    scrollToTopButton();
+
+    // 2 - Hide navigation bar while not scrolling
+    hideNavBar();
+
+    // 3 - Check the active section
+    checkActiveSection();
+});
